7c336fca1f41c611c93af2bc2cfbb7205b4fe8eb
Sajid <sajid.moinuddin@kayosports.com.au>
2019-09-02 22:06:04 +1000

    Add Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+  agent {
+    label "jenkins-maven"
+  }
+  environment {
+    CHART_REPOSITORY= 'https://artifactory.cluster.foxsports-gitops-prod.com.au/artifactory/helm' // Please do not edit this line! Managed by customize.sh
+    ORG = 'fsa-streamotion' 
+    APP_NAME = 'fsa-streamotion-java'
+  }
+
+  stages {
+    stage('Build Release') {
+      when {
+        branch 'master'
+      }
+      steps {
+        container('maven') {
+              // ensure we're not on a detached head
+              sh "git checkout master"
+              sh "git config --global credential.helper store"
+              sh "jx step git credentials"
+
+              sh "echo \$(jx-release-version) > VERSION"
+              sh "jx step tag --version \$(cat VERSION)"
+
+              sh "export VERSION=`cat ./../VERSION` && skaffold build -f skaffold.yaml"
+
+              script {
+                def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
+                currentBuild.description = "$buildVersion"
+                currentBuild.displayName = "$buildVersion"
+              }          
+        }
+      }
+    }
+  }
+  post {
+        always {
+          cleanWs()
+        }
+  }
+}
